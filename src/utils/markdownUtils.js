19411478283e89dab2e11688f7d4eb60052a1c06
Peter Wang <happypeter1983@gmail.com>
2018-05-16 11:43:49 +0800

    hightlight

--- src/utils/markdownUtils.js
+++ src/utils/markdownUtils.js
@@ -1,2 +1,16 @@
 import MarkdownIt from 'markdown-it'
-export default new MarkdownIt()
+import hljs from 'highlight.js'
+
+export default new MarkdownIt({
+  highlight: function(str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(lang, str).value
+      } catch (__) {
+        return null
+      }
+    }
+
+    return ''
+  }
+})
