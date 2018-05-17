dc380d649f0f5087232e7a05278cec143764a83a
Peter Wang <happypeter1983@gmail.com>
2018-05-17 11:50:15 +0800

    BlogHome style

--- src/components/BlogHome.vue
+++ src/components/BlogHome.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <h1>博客首页</h1>
+    <div class="hero">
+      博客
+    </div>
     <ul>
       <li v-for="post in postToc" :key="post.id" >
         <router-link :to="`/blog/${post.id}`">
@@ -24,3 +26,48 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.hero {
+  height: 194px;
+  font-size: 35px;
+  color: white;
+  text-align: center;
+  padding-top: 40px;
+  background: linear-gradient(
+    -90deg,
+    rgba(96, 190, 198, 1),
+    rgba(70, 156, 166, 1)
+  );
+}
+
+ul {
+  padding: 0;
+  margin: 20px auto;
+  width: 600px;
+  min-height: 40vh;
+}
+
+li {
+  list-style: none;
+}
+
+li {
+  background: rgba(255, 255, 255, 1);
+  border-radius: 7px;
+  margin-bottom: 20px;
+  padding: 10px;
+  padding-left: 20px;
+  box-shadow: 10px 12px 18px 0px rgba(231, 222, 222, 0.5);
+}
+
+li:hover {
+  box-shadow: 10px 12px 18px 0px rgba(176, 169, 169, 0.5);
+}
+
+li a {
+  font-size: 24px;
+  color: #000;
+  line-height: 60px;
+}
+</style>
