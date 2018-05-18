6b99e19a0445addba1d2e237f9df3edd818827e4
Peter Wang <happypeter1983@gmail.com>
2018-05-18 09:38:51 +0800

    tocArray

--- src/components/Ga.vue
+++ src/components/Ga.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>
-    相册
+  <div class="ga">
+     <div class="sidebar">
+       <ul>
+         <li v-for="t in tocList" :key="t.id">
+           <router-link :to="`/ga/${t.id}`">
+             {{ t.text }}
+           </router-link>
+         </li>
+       </ul>
+     </div>
   </div>
 </template>
+
+
+<script>
+import { tocArray } from '../constants/gaConstants'
+export default {
+  name: 'Ga',
+  computed: {
+    tocList: function() {
+      return tocArray
+    }
+  }
+}
+</script>
---
+++ src/constants/gaConstants.js
@@ -0,0 +1,15 @@
+export const toc = {
+  life: '生活',
+  work: '工作'
+}
+
+export const tocArray = [
+  {
+    id: 'life',
+    text: '生活'
+  },
+  {
+    id: 'work',
+    text: '工作'
+  }
+]
