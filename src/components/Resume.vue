8ca5c18e517fd82f6c41e1d85f051c87d33965c0
Peter Wang <happypeter1983@gmail.com>
2018-05-18 10:15:21 +0800

    resume layout

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,5 +1,34 @@
 <template>
-  <div>
-    简历
+  <div class="resume">
+    <div class="wrap">
+      Page
+    </div>
   </div>
 </template>
+
+<script>
+import Page from './ResumePage'
+export default {
+  name: 'resume',
+  components: {
+    Page
+  }
+}
+</script>
+
+<style scoped>
+.resume {
+  background-color: #f0f0f0;
+  min-height: 40vh;
+  display: flex;
+  flex-direction: column;
+}
+
+.wrap {
+  width: 600px;
+  background-color: #fff;
+  margin: 30px auto;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.4);
+  padding: 10px;
+}
+</style>
