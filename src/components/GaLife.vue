2a2175f958918dd046f999535600fffe880b003e
Peter Wang <happypeter1983@gmail.com>
2018-05-21 11:24:34 +0800

    masonry works

--- src/components/GaLife.vue
+++ src/components/GaLife.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-    <div v-masonry transition-duration="0.3s" item-selector=".item">
-      <div v-masonry-tile class="item" v-for="(item, index) in blocks">
+    <div v-masonry transition-duration="0.3s" item-selector=".item" class="grid">
+      <div class="grid-sizer"></div>
+      <div v-masonry-tile :key="index" class="item" v-for="(item, index) in blocks">
           <img :src="`/img/dog${item.id}.jpg`" />
       </div>
     </div>
@@ -12,27 +13,55 @@
 export default {
   name: 'GaLife',
   data: () => ({
-   blocks: [
-     {
+    blocks: [
+      {
         id: '1'
-     },
-     {
+      },
+      {
         id: '2'
-     },
-     {
+      },
+      {
         id: '3'
-     },
-     {
+      },
+      {
         id: '4'
-     },
-     {
+      },
+      {
         id: '5'
-     },
-     {
+      },
+      {
         id: '6'
-     }
-   ]
+      }
+    ]
   })
 }
 </script>
 
+<style scoped>
+.grid {
+  width: 500px;
+  height: 1000px;
+  background: #ddd;
+}
+
+/* clear fix */
+.grid:after {
+  content: '';
+  display: block;
+  clear: both;
+}
+
+.grid-sizer,
+.item {
+  width: 33.333%;
+}
+
+.item {
+  float: left;
+}
+
+.item img {
+  display: block;
+  max-width: 100%;
+}
+</style>
