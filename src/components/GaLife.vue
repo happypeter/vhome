4c532c333b6f64f49e44ab18db1f46509598d894
Peter Wang <happypeter1983@gmail.com>
2018-05-21 10:44:16 +0800

    add masonry

--- src/components/GaLife.vue
+++ src/components/GaLife.vue
@@ -1,5 +1,38 @@
 <template>
   <div>
-    Life
+    <div v-masonry transition-duration="0.3s" item-selector=".item">
+      <div v-masonry-tile class="item" v-for="(item, index) in blocks">
+          <img :src="`/img/dog${item.id}.jpg`" />
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'GaLife',
+  data: () => ({
+   blocks: [
+     {
+        id: '1'
+     },
+     {
+        id: '2'
+     },
+     {
+        id: '3'
+     },
+     {
+        id: '4'
+     },
+     {
+        id: '5'
+     },
+     {
+        id: '6'
+     }
+   ]
+  })
+}
+</script>
+
--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import router from './router'
 import App from './App.vue'
 import store from './store'
+import {VueMasonryPlugin} from 'vue-masonry'
+
+Vue.use(VueMasonryPlugin)
 
 Vue.config.productionTip = false
 
